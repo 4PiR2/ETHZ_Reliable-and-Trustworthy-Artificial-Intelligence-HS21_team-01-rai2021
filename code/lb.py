0f82058d72c17f54cb2e167377f3f61c33f08be2
jialchen <jialchen@student.ethz.ch>
2021-11-23 13:07:52 +0100

    spu bug fix

--- code/lb.py
+++ code/lb.py
@@ -11,8 +11,39 @@ def get_const(x, shape=1, device='cpu', ref=None):
 	else:
 		return x * torch.ones(shape, dtype=torch.float64, device=device)
 
-def case_0l(l, u, t=None):
-	# case 0 <= l
+def spu_0(x):
+	# spu function (optimized)
+	y = get_const(0., ref=x)
+	mask = x >= 0.
+	y[mask] = x[mask] ** 2 - .5
+	mask = ~mask
+	y[mask] = -torch.sigmoid(x[mask])
+	return y
+
+def spu_1(x):
+	# 1-order derivative
+	# spu'(0) := 0
+	y = get_const(0., ref=x)
+	mask = x >= 0.
+	y[mask] = 2. * x[mask]
+	mask = ~mask
+	f = lambda s: s * (s - 1.)
+	y[mask] = f(torch.sigmoid(x[mask]))
+	return y
+
+def get_tanget(t):
+	w = spu_1(t)
+	b = spu_0(t) - w * t
+	return w, b
+
+def get_scant(a, b):
+	ya, yb = spu_0(a), spu_0(b)
+	w = (yb - ya) / (b - a)
+	b = ya - w * a
+	return w, b
+
+def case_0ltu(l, u, t=None):
+	# case 0 <= l <= t <= u
 	# L: tangent @ t
 	# U: scant @ l, u
 	w_u = l + u
@@ -26,8 +57,8 @@ def case_0l(l, u, t=None):
 		b_l = - t ** 2 - .5
 	return w_l, b_l, w_u, b_u
 
-def case_u0(l, u, t=None):
-	# case u <= 0
+def case_ltu0(l, u, t=None):
+	# case l <= t <= u <= 0
 	# L: scant @ l, u
 	# U: tangent @ t
 	s_l = torch.sigmoid(l)
@@ -43,26 +74,33 @@ def case_u0(l, u, t=None):
 	return w_l, b_l, w_u, b_u
 
 def case_l0tu(l, u, t):
-	# case l < 0 <= t < u
+	# case l < 0 <= t <= u
 	# L: tangent @ t
-	# U: scant @ l, u
-	# warning: U is only verified with t <= (l + u) * .5
+	# U1: scant @ l, u
+	# U2: tangent @ l
 	w_l = 2. * t
 	b_l = - t ** 2 - .5
 	s_l = torch.sigmoid(l)
 	y_u = u ** 2 - .5
 	u_sub_l = u - l
 	w_u = (y_u + s_l) / u_sub_l
-	b_u = - (l * y_u + u * s_l) / u_sub_l
+	w_u_2 = s_l * (s_l - 1.)
+	mask = w_u >= w_u_2
+	b_u = get_const(0., ref=l)
+	b_u[mask] = - (l[mask] * y_u[mask] + u[mask] * s_l[mask]) / u_sub_l[mask]
+	mask = ~mask
+	w_u[mask] = w_u_2[mask]
+	b_u[mask] = - w_u_2[mask] * l[mask] - s_l[mask]
 	return w_l, b_l, w_u, b_u
 
 def case_lt0u(l, u, t):
-	# case l < t < 0 < u
+	# case l <= t < 0 < u
 	# L: scant @ l, 0
 	# U1: tangent @ t
 	# U2: scant @ l, u
 	# U3: tangent @ l
-	# u++ ==> U1 -> U3 -> U2
+	# U4: tangent @ a, solve s(a)(s(a) - 1.) = (u ** 2 - .5 + s(a)) / (u - a), not implemented
+	# u++ ==> U1 -> U4 -> U3 -> U2
 	s_l = torch.sigmoid(l)
 	w_l = (.5 - s_l) / l
 	b_l = get_const(-.5, ref=l)
@@ -74,7 +112,7 @@ def case_lt0u(l, u, t):
 	y_u = u[mask1] ** 2 - .5
 	u_sub_l = u[mask1] - l[mask1]
 	w_u_2 = (y_u + s_l[mask1]) / u_sub_l
-	w_u_3 = s_l[mask1] * (1. - s_l[mask1])
+	w_u_3 = s_l[mask1] * (s_l[mask1] - 1.)
 	# compare slope
 	mask2 = w_u_2 >= w_u_3
 	b_u_2 = get_const(0., ref=w_u_2)
@@ -108,38 +146,20 @@ def compute_linear_bounds(l, u):
 	w_u = get_const(0., ref=l)
 	b_u = get_const(0., ref=l)
 	mask1 = l >= 0.
-	w_l[mask1], b_l[mask1], w_u[mask1], b_u[mask1] = case_0l(l[mask1], u[mask1])
+	w_l[mask1], b_l[mask1], w_u[mask1], b_u[mask1] = case_0ltu(l[mask1], u[mask1])
 	mask2 = u <= 0.
-	w_l[mask2], b_l[mask2], w_u[mask2], b_u[mask2] = case_u0(l[mask2], u[mask2])
+	w_l[mask2], b_l[mask2], w_u[mask2], b_u[mask2] = case_ltu0(l[mask2], u[mask2])
 	mask3 = ~torch.bitwise_or(mask1, mask2)
 	w_l[mask3], b_l[mask3], w_u[mask3], b_u[mask3] = case_l0u(l[mask3], u[mask3])
 	return (w_l, b_l), (w_u, b_u)
-
-def spu_0(x):
-	# spu function (optimized)
-	y = get_const(0., ref=x)
-	mask = x >= 0.
-	y[mask] = x[mask] ** 2 - .5
-	mask = ~mask
-	y[mask] = -torch.sigmoid(x[mask])
-	return y
-
-def spu_1(x):
-	# 1-order derivative
-	# spu'(0) := 0
-	y = get_const(0., ref=x)
-	mask = x >= 0.
-	y[mask] = 2. * x[mask]
-	mask = ~mask
-	f = lambda s: s * (s - 1.)
-	y[mask] = f(torch.sigmoid(x[mask]))
-	return y
 
 
 if __name__ == '__main__':
 	# test
 	epsilon = 1e-10
 	n = 10000
+	visualize = 100
+
 	l, u = torch.randn(n, dtype=torch.float64), torch.randn(n, dtype=torch.float64)
 	l, u = torch.minimum(l, u), torch.maximum(l, u)
 	(w_l, b_l), (w_u, b_u) = compute_linear_bounds(l, u)
@@ -149,3 +169,17 @@ if __name__ == '__main__':
 	diff_u = w_u * x + b_u - y
 	assert torch.all(diff_l <= epsilon)
 	assert torch.all(diff_u >= -epsilon)
+
+	if visualize > 0:
+		import matplotlib.pyplot as plt
+		for i in range(visualize):
+			plt.plot(x[:, i], y[:, i], c='k')
+			plt.plot([l[i]] * 2, [w_l[i] * l[i] + b_l[i], w_u[i] * l[i] + b_u[i]], c='g')
+			plt.plot([u[i]] * 2, [w_l[i] * u[i] + b_l[i], w_u[i] * u[i] + b_u[i]], c='g')
+			plt.plot(x[:, i], w_l[i] * x[:, i] + b_l[i], c='b')
+			plt.plot(x[:, i], w_u[i] * x[:, i] + b_u[i], c='r')
+			plt.grid()
+			plt.title(str(i))
+			plt.savefig('../tmp/' + str(i) + '.jpg')
+			# plt.show()
+			plt.clf()
